5aa54f01edb8c7b5c31380bcca3cbc50469f4a65
tanisha <tanishamodi12@gmail.com>
2023-12-22 23:38:46 +0530

    accordion complete

---
+++ accordian/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Accordion from './Component/Accordion';
+
+function App() {
+  return (
+    <div className="App">
+       
+      <Accordion/>
+    </div>
+  );
+}
+
+export default App;
---
+++ accordian/src/Component/Accordion/index.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { useState } from "react";
+import data from "./data";
+import "./style.css";
+
+function Accordion() {
+  const [selected, setSelected] = useState(null);
+  const [enableMultiSelection, setEnableMultiSelection] = useState(false);
+  const [multiple, setMultiple] = useState([]);
+
+  const handleSingleSelection = (getCurrentId) => {
+    console.log(getCurrentId);
+    setSelected(getCurrentId === selected ? null : getCurrentId);
+  };
+  const handleMultiSeletion = (getCurrentId) => {
+    let cpyMultiple = [...multiple];
+    const findIndexOfCurrentId = cpyMultiple.indexOf(getCurrentId);
+    console.log(findIndexOfCurrentId);
+    if (findIndexOfCurrentId === -1) cpyMultiple.push(getCurrentId);
+    else cpyMultiple.splice(findIndexOfCurrentId, 1);
+    setMultiple(cpyMultiple);
+  };
+  console.log(selected, multiple);
+  return (
+    <div className="wrapper">
+      <button
+        onClick={() => {
+          setEnableMultiSelection(!enableMultiSelection);
+          console.log(enableMultiSelection);
+        }}
+      >
+        Enable multi selection
+      </button>
+      <div className="accordion">
+        {data && data.length > 0 ? (
+          data.map((dataItem) => (
+            <div className="item">
+              <div
+                onClick={
+                  enableMultiSelection
+                    ? () => handleMultiSeletion(dataItem.id)
+                    : () => handleSingleSelection(dataItem.id)
+                }
+                className="title"
+              >
+                <h3>{dataItem.question}</h3>
+                <span>+</span>
+              </div>
+              {enableMultiSelection
+                ? multiple.indexOf(dataItem.id) !== -1 && (
+                    <div className="content">{dataItem.answer}</div>
+                  )
+                : selected === dataItem.id && (
+                    <div className="content">{dataItem.answer}</div>
+                  )}
+              {/* {selected === dataItem.id ? (
+                <div className="content">{dataItem.answer}</div>
+              ) : null} */}
+            </div>
+          ))
+        ) : (
+          <div>No data found!</div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Accordion;
