daf43187cc9fd02510e4d9065a26fdfce741a17b
tanisha <tanishamodi12@gmail.com>
2023-12-24 00:10:04 +0530

    Random-color complete

--- accordian/src/App.js
+++ accordian/src/App.js
@@ -1,12 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import Accordion from './Component/Accordion';
+import Random_color from './Component/Random_color';
 
 function App() {
   return (
     <div className="App">
-       
+
+       {/* Accordion Component */}
       <Accordion/>
+
+      {/* Random Color Component */}
+      <Random_color/>
+
     </div>
   );
 }
